d1656a6bfacd17eae0af48d3937b611807792dbe
Fresinsya <vicafresinsya@gmail.com>
2023-11-09 13:29:10 +0700

    insert user

---
+++ User/insertUser.sql
@@ -0,0 +1,13 @@
+insert INTO `user` (`name`, `username`, `password`) VALUES 
+('John Doe', 'john.doe', '12345'),
+('Jane Smith', 'jane.smith', 'smithy987'),
+('Michael Johnson', 'mike.j', 'mjpass456'),
+('Emily Davis', 'emily.d', 'password789'),
+('David Wilson', 'david.w', 'wilson123'),
+('Sarah Brown', 'sarah.b', 'sbrown456'),
+('Alex Turner', 'alex.t', 'alexpass789'),
+('Olivia Miller', 'olivia.m', 'miller987'),
+('Ethan White', 'ethan.w', 'ethan123'),
+('Sophia Clark', 'sophia.c', 'sophiapass456');
+
+SELECT * from `user`;
