91ab6fdf96540009ec237b59c2910885369390df
Fresinsya <vicafresinsya@gmail.com>
2023-11-09 13:20:36 +0700

    first commit

---
+++ createDatabase.sql
@@ -0,0 +1,4 @@
+-- Active: 1699509845435@@127.0.0.1@3306@mentoring
+create DATABASE online_retail;
+
+use online_retail;
---
+++ createTabel.sql
@@ -0,0 +1,48 @@
+-- Active: 1699509845435@@127.0.0.1@3306@online_retail
+create Table User (
+    id int not null primary key auto_increment,
+    name varchar(255) not null,
+    username varchar(255) not null,
+    password varchar(255) not null
+);
+
+create Table Cart (
+    id int not null primary key auto_increment,
+    id_user int not null,
+    id_produk int not null,
+    quantity int not null,
+    foreign key (id_user) references User(id)
+);
+
+create Table Kategori (
+    id int not null primary key auto_increment,
+    namaKategori varchar(255) not null
+);
+
+create table Produk (
+    id int not null primary key auto_increment,
+    namaBarang varchar(255) not null,
+    harga int not null,
+    stok int not null,
+    id_kategori int not null,
+    foreign key (id_kategori) references Kategori(id)
+);
+
+create Table Transaksi (
+    id int not null primary key auto_increment,
+    id_user int not null,
+    total int not null,
+    foreign key (id_user) references User(id)
+);
+
+create TABLE DetailTransaksi (
+    id int not null primary key auto_increment,
+    id_transaksi int not null,
+    id_produk int not null,
+    quantity int not null,
+    foreign key (id_transaksi) references Transaksi(id),
+    foreign key (id_produk) references Produk(id)
+)
+
+
+show tables;
